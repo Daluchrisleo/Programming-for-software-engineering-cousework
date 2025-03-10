dc2379d690c629d4f9ffcee942c9ff03261e3957
Chukwudalu Ibuodinma <ci23aad@herts.ac.uk>
2025-03-10 16:07:16 +0000

    Created boost physio clinic class

---
+++ BoostPhysioClinic/src/main/java/com/boostphysioclinic/BoostPhysioClinic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.boostphysioclinic;
+
+/**
+ *
+ * @author DELL
+ */
+public class BoostPhysioClinic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
