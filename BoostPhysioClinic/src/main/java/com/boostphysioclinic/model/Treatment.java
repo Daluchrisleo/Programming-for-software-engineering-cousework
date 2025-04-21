6cb1aaf90a7fb24315d3a40c8e9f19e0727ab5b2
Chukwudalu Ibuodinma <ci23aad@herts.ac.uk>
2025-04-21 22:52:53 +0100

    Added doc strings to the Treatment class

--- BoostPhysioClinic/src/main/java/com/boostphysioclinic/model/Treatment.java
+++ BoostPhysioClinic/src/main/java/com/boostphysioclinic/model/Treatment.java
@@ -1,17 +1,39 @@
 package com.boostphysioclinic.model;
 
 
+/**
+ * Represents a type of treatment that can be scheduled in a timetable slot.
+ * Each treatment has a name describing the procedure or therapy.
+ */
 public class Treatment {
     private String name;
 
+    /**
+     * Constructs a new {@code Treatment} with the specified name.
+     *
+     * @param name the name of the treatment
+     */
     public Treatment(String name) {
         this.name = name;
     }
 
-    public String getName() { return name; }
+    /**
+     * Returns the name of the treatment.
+     *
+     * @return the treatment name
+     */
+    public String getName() {
+        return name;
+    }
 
+    /**
+     * Returns the string representation of the treatment.
+     *
+     * @return the treatment name
+     */
     @Override
     public String toString() {
         return getName();
     }
 }
+
