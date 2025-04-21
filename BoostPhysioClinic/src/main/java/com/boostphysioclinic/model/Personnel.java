967524288b7b548190fede4cefe645ca1e8cf6d9
Chukwudalu Ibuodinma <ci23aad@herts.ac.uk>
2025-04-21 22:48:20 +0100

    Added doc strings to the Personnel class

--- BoostPhysioClinic/src/main/java/com/boostphysioclinic/model/Personnel.java
+++ BoostPhysioClinic/src/main/java/com/boostphysioclinic/model/Personnel.java
@@ -1,12 +1,24 @@
 package com.boostphysioclinic.model;
 
-
+/**
+ * An abstract class representing personnel within the system.
+ * Serves as a base for specific types of personnel such as patients or staff,
+ * providing common fields and access methods.
+ */
 public abstract class Personnel {
     private int id;
     private String fullName;
     private String address;
     private String tel;
 
+    /**
+     * Constructs a Personnel instance with the specified attributes.
+     *
+     * @param id       the unique identifier for the personnel
+     * @param fullName the full name of the personnel
+     * @param address  the address of the personnel
+     * @param tel      the telephone number of the personnel
+     */
     public Personnel(int id, String fullName, String address, String tel) {
         this.id = id;
         this.fullName = fullName;
@@ -14,18 +26,38 @@ public abstract class Personnel {
         this.tel = tel;
     }
 
+    /**
+     * Returns the unique ID of the personnel.
+     *
+     * @return the ID
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Returns the full name of the personnel.
+     *
+     * @return the full name
+     */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * Returns the address of the personnel.
+     *
+     * @return the address
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Returns the telephone number of the personnel.
+     *
+     * @return the telephone number
+     */
     public String getTel() {
         return tel;
     }
